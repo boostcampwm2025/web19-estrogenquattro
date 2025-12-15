91e245d87dd57740610c547bb698e361cf6d6759
heisjun <jun146517@gmail.com>
2025-12-15 21:58:28 +0900

    chore: Phaser 환경 구성
    - Phaser 호환성을 위한 Webpack 설정 추가 및 빌드 스크립트 수정

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {},
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      canvas: false,
+      fs: false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
