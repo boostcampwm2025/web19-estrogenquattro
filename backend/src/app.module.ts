77b22cba4874231d87bf253cf7e86a7333e4df86
songhaechan <bukak2019@naver.com>
2025-12-16 09:26:08 +0900

    chore: 임포트 에러 해결

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CharacterModule } from './character/character.module';
-import { CharactersModule } from './characters/characters.module';
-import { CharaterGateway } from './charater/charater.gateway';
-import { CharacterModule } from './character/character.module';
+import {CharacterModule} from "./character/character.module";
+import {CharacterGateway} from "./character/character.gateway";
 
 @Module({
-  imports: [CharacterModule, CharactersModule],
+  imports: [CharacterModule],
   controllers: [AppController],
-  providers: [AppService, CharaterGateway],
+  providers: [AppService, CharacterGateway],
 })
 export class AppModule {}
