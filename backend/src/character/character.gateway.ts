6ceae25b8ad830cbf8c1d99cf05194a85a3383ba
songhaechan <bukak2019@naver.com>
2025-12-16 23:40:46 +0900

    feat: 방에 접속할 경우 타이머 시작

--- backend/src/character/character.gateway.ts
+++ backend/src/character/character.gateway.ts
@@ -1,9 +1,17 @@
-import {ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway} from '@nestjs/websockets';
-import {Socket} from "socket.io";
-import {MoveReq} from "./dto/move.dto";
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { MoveReq } from './dto/move.dto';
+import { CharacterService } from './character.service';
 
 @WebSocketGateway()
 export class CharacterGateway {
+  constructor(private readonly characterService: CharacterService) {}
+
   @SubscribeMessage('moving')
   handleMove(
     @MessageBody()
@@ -13,6 +21,17 @@ export class CharacterGateway {
     client.to(data.roomId).emit('moved', {
       userId: client.id,
       direction: data.direction,
+    });
+  }
+
+  @SubscribeMessage('joining')
+  handleJoinRoom(@ConnectedSocket() client: Socket) {
+    this.characterService.startSessionTimer(client.id, (minutes) => {
+      // roomId는 추후 방 배정 이후 수정 필요
+      client.to('roomId').emit('timerUpdated', {
+        userId: client.id,
+        minutes,
+      });
     });
   }
 }
