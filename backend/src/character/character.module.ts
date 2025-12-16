592ea0209a15841364409b482d32f4a47b5b6f20
songhaechan <bukak2019@naver.com>
2025-12-16 23:41:14 +0900

    feat: 모듈에 Service 프로바이더 등록

--- backend/src/character/character.module.ts
+++ backend/src/character/character.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import {CharacterGateway} from "./character.gateway";
+import { CharacterService } from './character.service';
 
 @Module({
-  providers: [CharacterGateway]
+  providers: [CharacterGateway, CharacterService]
 })
 export class CharacterModule {}
