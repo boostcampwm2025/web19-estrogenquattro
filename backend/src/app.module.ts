2ee844c2d6de43ed7dabafec0ce16f9b9c779fc8
songhaechan <bukak2019@naver.com>
2025-12-17 18:24:34 +0900

    refactor: AppModule에 PlayTimeService 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlayerModule } from './player/player.module';
 import { PlayerGateway } from './player/player.gateway';
+import { PlayTimeService } from './player/player.play-time-service';
 
 @Module({
   imports: [PlayerModule],
   controllers: [AppController],
-  providers: [AppService, PlayerGateway],
+  providers: [AppService, PlayerGateway, PlayTimeService],
 })
 export class AppModule {}
