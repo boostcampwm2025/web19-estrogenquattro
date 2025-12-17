a84c48df5a000eceefafee68febe825e97462714
JunHyuck Kim <82956755+heisjun@users.noreply.github.com>
2025-12-17 16:43:59 +0900

    Feat/#15 character sync (#30)
    * feat: MoveReq dto 변경
    
    프론트엔드 스펙에 맞춰 업데이트
    
    * feat: cors 정책 추가
    
    * feat: socket 이동처리 로직 추가
    
    - 일단 임시로 접속한 플레이어들의 상태를 메모리에 저장
    
    * feat: 플레이어 소켓 접속로직 추가

--- backend/src/character/dto/move.dto.ts
+++ backend/src/character/dto/move.dto.ts
@@ -2,5 +2,10 @@ import { Direction } from '../types/direction.type';
 
 export class MoveReq {
   roomId: string;
+  userId: string;
+  x: number;
+  y: number;
+  isMoving: boolean;
   direction: Direction;
+  timestamp: number;
 }
