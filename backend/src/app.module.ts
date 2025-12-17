c76b029dd19c1ad4adf20e77c25780db254d90bf
songhaechan <bukak2019@naver.com>
2025-12-17 23:37:13 +0900

    feat: 모듈에 GithubGateway 등록

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,10 +6,17 @@ import { PlayerGateway } from './player/player.gateway';
 import { PlayTimeService } from './player/player.play-time-service';
 import { GithubModule } from './github/github.module';
 import { GithubPollService } from './github/github.poll-service';
+import { GithubGateway } from './github/github.gateway';
 
 @Module({
   imports: [PlayerModule, GithubModule],
   controllers: [AppController],
-  providers: [AppService, PlayerGateway, PlayTimeService, GithubPollService],
+  providers: [
+    AppService,
+    PlayerGateway,
+    PlayTimeService,
+    GithubPollService,
+    GithubGateway,
+  ],
 })
 export class AppModule {}
--- backend/src/player/player.module.ts
+++ backend/src/player/player.module.ts
@@ -3,9 +3,10 @@ import { PlayerGateway } from './player.gateway';
 import { PlayTimeService } from './player.play-time-service';
 import { GithubModule } from '../github/github.module';
 import { GithubPollService } from '../github/github.poll-service';
+import { GithubGateway } from '../github/github.gateway';
 
 @Module({
   imports: [GithubModule],
-  providers: [PlayerGateway, PlayTimeService, GithubPollService],
+  providers: [PlayerGateway, PlayTimeService, GithubPollService, GithubGateway],
 })
 export class PlayerModule {}
