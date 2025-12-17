66e70b9b1ad093e45327e0e5623b6eb8e5035883
heisjun <jun146517@gmail.com>
2025-12-17 16:18:17 +0900

    feat: Socket.io 클라이언트 관리 모듈(SocketManager) 구현

---
+++ frontend/src/game/SocketManager.ts
@@ -0,0 +1,40 @@
+import { io, Socket } from "socket.io-client";
+
+let socket: Socket | null = null;
+
+export const connectSocket = (url: string) => {
+  if (socket) {
+    if (socket.connected) return socket;
+    socket = null;
+  }
+
+  socket = io(url, {
+    transports: ["websocket"],
+    autoConnect: true,
+  });
+
+  socket.on("connect", () => {
+    console.log("Connected to socket server:", socket?.id);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected from socket server");
+  });
+
+  return socket;
+};
+
+export const disconnectSocket = () => {
+  if (socket) {
+    socket.disconnect();
+    socket = null;
+  }
+};
+
+export const getSocket = () => socket;
+
+export const emitEvent = (event: string, data: any) => {
+  if (socket && socket.connected) {
+    socket.emit(event, data);
+  }
+};
