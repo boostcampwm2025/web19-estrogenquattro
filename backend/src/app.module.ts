9b60c41edf4008f8f6ba5214d6589006d8ea675c
songhaechan <bukak2019@naver.com>
2025-12-17 23:32:52 +0900

    feat: 모듈 등록
    - GithubModule 과 연관된 프로바이더 등록

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { PlayerModule } from './player/player.module';
 import { PlayerGateway } from './player/player.gateway';
 import { PlayTimeService } from './player/player.play-time-service';
+import { GithubModule } from './github/github.module';
+import { GithubPollService } from './github/github.poll-service';
 
 @Module({
-  imports: [PlayerModule],
+  imports: [PlayerModule, GithubModule],
   controllers: [AppController],
-  providers: [AppService, PlayerGateway, PlayTimeService],
+  providers: [AppService, PlayerGateway, PlayTimeService, GithubPollService],
 })
 export class AppModule {}
--- backend/src/player/player.module.ts
+++ backend/src/player/player.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PlayerGateway } from './player.gateway';
 import { PlayTimeService } from './player.play-time-service';
+import { GithubModule } from '../github/github.module';
+import { GithubPollService } from '../github/github.poll-service';
 
 @Module({
-  providers: [PlayerGateway, PlayTimeService],
+  imports: [GithubModule],
+  providers: [PlayerGateway, PlayTimeService, GithubPollService],
 })
 export class PlayerModule {}
