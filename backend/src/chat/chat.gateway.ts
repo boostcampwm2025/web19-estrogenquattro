07e448617a86711bcd80413d3573f2cc1973f890
songhaechan <bukak2019@naver.com>
2026-01-05 20:26:07 +0900

    feat: 채팅 브로드 캐스팅
    - 같은 방에 존재하는 사람들에게 채팅을 브로드 캐스팅
    - 채팅 내역을 유지하지 않도록 구성

---
+++ backend/src/chat/chat.gateway.ts
@@ -0,0 +1,21 @@
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { RoomService } from '../room/room.service';
+
+@WebSocketGateway()
+export class ChatGateway {
+  constructor(private readonly roomService: RoomService) {}
+  @SubscribeMessage('chatting')
+  handleMessage(
+    @MessageBody()
+    data: {
+      message: string;
+    },
+    @ConnectedSocket()
+    client: Socket,
+  ) {
+    const roomId = this.roomService.getRoomIdBySocketId(client.id);
+    if (!roomId) return;
+    client.to(roomId).emit('chatted', { userId: client.id, message: data.message });
+  }
+}
