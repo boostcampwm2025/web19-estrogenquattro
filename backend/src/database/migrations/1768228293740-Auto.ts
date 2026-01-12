2872549f88b219ecda442285f0f8693eef06f0be
songhaechan <bukak2019@naver.com>
2026-01-12 23:34:21 +0900

    fix: 마이그레이션 파일 lint에러

--- backend/src/database/migrations/1768228293740-Auto.ts
+++ backend/src/database/migrations/1768228293740-Auto.ts
@@ -1,16 +1,19 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Auto1768228293740 implements MigrationInterface {
-    name = 'Auto1768228293740'
+  name = 'Auto1768228293740';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "tasks" ("id" bigint PRIMARY KEY AUTOINCREMENT NOT NULL, "player_id" bigint, "description" varchar(100), "duration_minutes" integer NOT NULL DEFAULT (0), "completed_date" date NOT NULL, "created_date" date NOT NULL)`);
-        await queryRunner.query(`CREATE TABLE "players" ("id" bigint PRIMARY KEY AUTOINCREMENT NOT NULL, "social_id" bigint NOT NULL, "nickname" varchar(20) NOT NULL, "primary_user_pet_id" bigint, "total_point" integer NOT NULL DEFAULT (0), CONSTRAINT "UQ_e304f5048f92add68e1f8c0ce03" UNIQUE ("social_id"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "tasks" ("id" bigint PRIMARY KEY AUTOINCREMENT NOT NULL, "player_id" bigint, "description" varchar(100), "duration_minutes" integer NOT NULL DEFAULT (0), "completed_date" date NOT NULL, "created_date" date NOT NULL)`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "players" ("id" bigint PRIMARY KEY AUTOINCREMENT NOT NULL, "social_id" bigint NOT NULL, "nickname" varchar(20) NOT NULL, "primary_user_pet_id" bigint, "total_point" integer NOT NULL DEFAULT (0), CONSTRAINT "UQ_e304f5048f92add68e1f8c0ce03" UNIQUE ("social_id"))`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "players"`);
-        await queryRunner.query(`DROP TABLE "tasks"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "players"`);
+    await queryRunner.query(`DROP TABLE "tasks"`);
+  }
 }
