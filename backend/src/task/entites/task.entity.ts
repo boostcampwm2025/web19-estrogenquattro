bf8d2230704fe9a204382000631fd433e1f99913
songhaechan <bukak2019@naver.com>
2026-01-13 22:28:39 +0900

    refactor: 엔티티 필드 네이밍 변경
    - durationMinutes -> totalFocusMinutes 의미가 더 잘드러나도록 변경

--- backend/src/task/entites/task.entity.ts
+++ backend/src/task/entites/task.entity.ts
@@ -1,4 +1,10 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
 import { Player } from '../../player/entites/player.entity';
 
 @Entity('tasks')
@@ -7,13 +13,14 @@ export class Task {
   id: number;
 
   @ManyToOne(() => Player)
+  @JoinColumn({ name: 'player_id' })
   player: Player;
 
   @Column({ type: 'varchar', length: 100, nullable: false })
   description: string;
 
-  @Column({ type: 'int', name: 'duration_minutes', default: 0 })
-  durationMinutes: number;
+  @Column({ type: 'int', name: 'total_focus_minutes', default: 0 })
+  totalFocusMinutes: number;
 
   @Column({ type: 'date', name: 'completed_date', nullable: true })
   completedDate: Date | null;
