fc5c1544423d759597171a8b5e7241872fb9f96e
songhaechan <bukak2019@naver.com>
2026-01-13 11:57:57 +0900

    fix: front 개발서버 실행 스크립트 변경

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,17 @@
-  module.exports = {
-    apps: [
-      {
-        name: 'backend',
-        cwd: './backend',
-        script: 'dist/main.js',
-        instances: 1,
-      },
-      {
-        name: 'frontend',
-        cwd: './frontend',
-        script: 'node_modules/next/dist/bin/next',
-        args: 'start -p 3000',
-        instances: 1,
-      }
-    ]
-  }
+module.exports = {
+  apps: [
+    {
+      name: 'backend',
+      cwd: './backend',
+      script: 'dist/main.js',
+      instances: 1,
+    },
+    {
+      name: 'frontend',
+      cwd: './frontend',
+      script: 'npm run dev',
+      args: 'start -p 3000',
+      instances: 1,
+    }
+  ]
+}
