6889ba2cc436b4df786fc67ac134e3b67869001a
songhaechan <bukak2019@naver.com>
2026-01-13 16:14:09 +0900

    feat: palyer 스키마 변경
    - socialId, primaryUserPetId 데이터 타입 변경

--- backend/src/player/entites/player.entity.ts
+++ backend/src/player/entites/player.entity.ts
@@ -6,13 +6,13 @@ export class Player {
   id: number;
 
   @Column({ type: 'bigint', name: 'social_id', unique: true, nullable: false })
-  socialId: string;
+  socialId: number;
 
   @Column({ type: 'varchar', length: 20, nullable: false })
   nickname: string;
 
   @Column({ type: 'bigint', name: 'primary_user_pet_id', nullable: true })
-  primaryUserPetId: string;
+  primaryUserPetId: number;
 
   @Column({ type: 'int', name: 'total_point', default: 0 })
   totalPoint: number;
