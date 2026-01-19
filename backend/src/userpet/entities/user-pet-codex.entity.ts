9c3b755f41126f6dd1d456cca916e5bc753375c2
heisjun <jun146517@gmail.com>
2026-01-19 22:57:42 +0900

    feat: 펫 도감 엔티티 생성

---
+++ backend/src/userpet/entities/user-pet-codex.entity.ts
@@ -0,0 +1,33 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+} from 'typeorm';
+import { Player } from '../../player/entites/player.entity';
+import { Pet } from './pet.entity';
+
+@Entity('user_pet_collections')
+export class UserPetCodex {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'int', name: 'player_id' })
+  playerId: number;
+
+  @Column({ type: 'int', name: 'pet_id' })
+  petId: number;
+
+  @CreateDateColumn({ name: 'acquired_at' })
+  acquiredAt: Date;
+
+  @ManyToOne(() => Player)
+  @JoinColumn({ name: 'player_id' })
+  player: Player;
+
+  @ManyToOne(() => Pet)
+  @JoinColumn({ name: 'pet_id' })
+  pet: Pet;
+}
