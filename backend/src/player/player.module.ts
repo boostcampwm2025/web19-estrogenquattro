88128d0a3f01871616b0089144c781752200b273
heisjun <jun146517@gmail.com>
2026-01-20 23:27:35 +0900

    feat: 사용자 펫 장착 비즈리스 로직 구현
    - `GET /api/players/me/info`: 플레이어 기본 정보 조회 (현재 무슨펫을 장착하고 있는가)
      - `PATCH /api/players/me/equipped-pet`: 대표 펫 장착 기능( 도감에서 대표펫 설정시)
      - 플레이어 관련 API를 통합 관리하기위해 순환참조를 이용

--- backend/src/player/player.module.ts
+++ backend/src/player/player.module.ts
@@ -2,12 +2,14 @@ import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '../auth/auth.module';
 import { PlayerGateway } from './player.gateway';
+import { PlayerController } from './player.controller';
 import { PlayerService } from './player.service';
 import { GithubModule } from '../github/github.module';
 import { RoomModule } from '../room/room.module';
 import { Player } from './entites/player.entity';
 
 import { FocusTimeModule } from '../focustime/focustime.module';
+import { PetModule } from '../userpet/pet.module';
 
 @Module({
   imports: [
@@ -16,7 +18,9 @@ import { FocusTimeModule } from '../focustime/focustime.module';
     RoomModule,
     forwardRef(() => AuthModule),
     forwardRef(() => FocusTimeModule),
+    PetModule,
   ],
+  controllers: [PlayerController],
   providers: [PlayerGateway, PlayerService],
   exports: [TypeOrmModule, PlayerService],
 })
---
+++ backend/src/userpet/dto/equip-pet.dto.ts
@@ -0,0 +1,3 @@
+export class EquipPetDto {
+  petId: number;
+}
