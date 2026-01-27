48a27298bbad2a7ea6e32998e271d512f3d57ff2
songhaechan <bukak2019@naver.com>
2026-01-27 11:20:13 +0900

    refactor: 사용하지않는 코드 제거
    - addHistory
    - historyRepository

--- backend/src/pointhistory/point-history.service.ts
+++ backend/src/pointhistory/point-history.service.ts
@@ -5,25 +5,6 @@ import { PointHistory, PointType } from './entities/point-history.entity';
 
 @Injectable()
 export class PointHistoryService {
-  constructor(
-    @InjectRepository(PointHistory)
-    private readonly pointHistoryRepository: Repository<PointHistory>,
-  ) {}
-
-  async addHistory(
-    playerId: number,
-    type: PointType,
-    amount: number,
-  ): Promise<PointHistory> {
-    const history = this.pointHistoryRepository.create({
-      player: { id: playerId },
-      type,
-      amount,
-    });
-
-    return this.pointHistoryRepository.save(history);
-  }
-
   async addHistoryWithManager(
     manager: EntityManager,
     playerId: number,
