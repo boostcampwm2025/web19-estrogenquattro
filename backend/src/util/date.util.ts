119bd6acc64c3b5725dda1e92f7b737b4168447b
songhaechan <bukak2019@naver.com>
2026-01-28 16:04:48 +0900

    chore: 사용하지 않는 메서드 제거

--- backend/src/util/date.util.ts
+++ backend/src/util/date.util.ts
@@ -4,24 +4,6 @@ export interface DateRange {
 }
 
 /**
- * 오늘의 범위를 반환
- * 시작: 오늘 UTC 15:00
- * 끝: 내일 UTC 14:59:59.999
- */
-export function getTodayRange(): DateRange {
-  const now = new Date();
-
-  const start = new Date(now);
-  start.setUTCHours(15, 0, 0, 0);
-
-  const end = new Date(now);
-  end.setUTCDate(end.getUTCDate() + 1);
-  end.setUTCHours(14, 59, 59, 999);
-
-  return { start, end };
-}
-
-/**
  * 어제의 범위를 반환 (UTC 15시에 호출되는 스케줄러용)
  * 시작: 어제 UTC 15:00
  * 끝: 오늘 UTC 14:59:59.999
