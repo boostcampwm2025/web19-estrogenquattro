bdc612c73672df6dd076e394a6c6ecce436fd8e2
heisjun <jun146517@gmail.com>
2026-02-27 15:40:09 +0900

    feat: 버그 제보 버튼 구현

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,8 +5,10 @@ import UserInfoModal from "./_components/UserInfoModal";
 import FocusPanel from "./_components/FocusPanel";
 import LeaderboardModal from "./_components/LeaderboardModal";
 import ChannelSelectModal from "./_components/ChannelSelectModal";
+import BugReportModal from "./_components/BugReportModal";
 import LeaderboardButton from "./_components/LeaderboardButton";
 import ChannelSelectButton from "./_components/ChannelSelectButton";
+import BugReportButton from "./_components/BugReportButton";
 import UserInfoButton from "./_components/UserInfoButton";
 import ProgressBar from "@/_components/ui/ProgressBar";
 import { OnboardingTour } from "./_components/OnboardingTour";
@@ -22,15 +24,21 @@ export default function Home() {
           <UserInfoModal />
           <LeaderboardModal />
           <ChannelSelectModal />
+          <BugReportModal />
           <OnboardingTour />
         </ClientOnly>
         <div className="absolute top-4 right-4 z-40">
           <FocusPanel />
         </div>
-        <div className="absolute top-4 left-4 z-30 flex flex-col gap-4">
-          <UserInfoButton />
-          <LeaderboardButton />
-          <ChannelSelectButton />
+        <div className="absolute top-4 left-4 z-30 flex h-full flex-col justify-between">
+          <div className="flex flex-col gap-4">
+            <UserInfoButton />
+            <LeaderboardButton />
+            <ChannelSelectButton />
+          </div>
+          <div className="mb-8">
+            <BugReportButton />
+          </div>
         </div>
         <ConnectionLostOverlay />
       </div>
