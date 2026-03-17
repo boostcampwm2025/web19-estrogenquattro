759aeec1c429f5cd9c4fde496bee34068dd1c7d0
hoon <hoon@hoonui-Macmini.local>
2026-03-17 21:59:25 +0900

    fix: 전체 단위 테스트 커버리지 기준으로 조정

--- backend/jest.unit.config.cjs
+++ backend/jest.unit.config.cjs
@@ -2,16 +2,7 @@ const packageJson = require("./package.json");
 
 module.exports = {
   ...packageJson.jest,
-  collectCoverageFrom: [
-    "app.controller.ts",
-    "auth/user.store.ts",
-    "chat/chat.gateway.ts",
-    "focustime/focustime.service.ts",
-    "player/player.service.ts",
-    "point/point.service.ts",
-    "pointhistory/point-history.service.ts",
-    "task/task.service.ts",
-  ],
+  collectCoverageFrom: ["**/*.ts", "!**/*.spec.ts", "!test/**"],
   coverageReporters: ["json-summary", "lcov", "text-summary"],
   coverageThreshold: {
     global: {
