b866895a0b802641031931e392401eb76160a765
heisjun <jun146517@gmail.com>
2026-03-17 16:49:26 +0900

    feat: PublicNotificationController를 NotificationModule에 등록

--- backend/src/notification/notification.module.ts
+++ backend/src/notification/notification.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Notification } from './entities/notification.entity';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
+import { PublicNotificationController } from './public-notification.controller';
 import { AdminModule } from '../admin/admin.module';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Notification]), AdminModule, AuthModule],
-  controllers: [NotificationController],
+  controllers: [NotificationController, PublicNotificationController],
   providers: [NotificationService],
 })
 export class NotificationModule {}
